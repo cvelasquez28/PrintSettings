12d9526607340947bbf0ccca8a4c6eb964048022
Cindy Velásquez <39968707+cvelasquez28@users.noreply.github.com>
2021-10-12 12:42:15 -0500

    Create PrintSettings.java

---
+++ PrintSettings.java
@@ -0,0 +1,77 @@
+public class PrintSettings {
+
+    private static OutputStream outputStream;
+
+    public PrintSettings(OutputStream os) {
+        this.outputStream = os;
+    }
+
+    public void printCustom(String msg, int size, int align) {
+
+
+        //Print config "mode"
+        byte[] cc = new byte[]{0x1B, 0x21, 0x03};  // 0- normal size text
+        //byte[] cc1 = new byte[]{0x1B,0x21,0x00};  // 0- normal size text
+        byte[] bb = new byte[]{0x1B, 0x21, 0x08};  // 1- only bold text
+        byte[] bb2 = new byte[]{0x1B, 0x21, 0x20}; // 2- bold with medium text
+        byte[] bb3 = new byte[]{0x1B, 0x21, 0x10}; // 3- bold with large text
+
+        try {
+            switch (size) {
+                case 0:
+                    outputStream.write(cc);
+                    break;
+                case 1:
+                    outputStream.write(bb);
+                    break;
+                case 2:
+                    outputStream.write(bb2);
+                    break;
+                case 3:
+                    outputStream.write(bb3);
+                    break;
+            }
+
+            switch (align) {
+                case 0:
+                    //left align
+                    outputStream.write(PrinterCommands.ESC_ALIGN_LEFT);
+                    break;
+                case 1:
+                    //center align
+                    outputStream.write(PrinterCommands.ESC_ALIGN_CENTER);
+                    break;
+                case 2:
+                    //right align
+                    outputStream.write(PrinterCommands.ESC_ALIGN_RIGHT);
+                    break;
+            }
+            outputStream.write(msg.getBytes());
+            outputStream.write(PrinterCommands.LF);
+            //outputStream.write(cc);
+            //printNewLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void printNewLine() {
+        try {
+            outputStream.write(PrinterCommands.FEED_LINE);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void printText(byte[] msg) {
+        try {
+            // Print normal text
+            outputStream.write(msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
